1f2a9a6d766c08705a557ea10b6ec5398b969bcf
Siddharth Nagpure <61226004+SiddharthNagpure@users.noreply.github.com>
2020-06-23 00:02:36 +0530

    Update and rename code.py to student_management_system.py

--- GreyAtom-Python-Tutorial/student_management_system.py
+++ GreyAtom-Python-Tutorial/student_management_system.py
@@ -4,11 +4,14 @@
 # Create the lists 
 class_1 = ['Geoffrey', 'Hinton', 'Andrew Ng', 'Sebastian Raschka', 'Yoshua Bengio']
 class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+
 # Concatenate both the strings
 new_class = class_1+class_2
 print(new_class)
+
 # Append the list
 new_class.append('Peter Warden')
+
 # Print updated list
 print(new_class)
 
@@ -31,24 +34,25 @@ print(total/500*100)
 # Create the Dictionary
 mathematics = {'Geoffrey Hinton':78, 'Andrew Ng':95, 'Sebastian Raschka':65, 'Yoshua Benjio':50, 'Hilary Mason':70, 'Corinna Cortes':66, 'Peter Warden':75}
 
-# Given string
 topper = max(mathematics,key = mathematics.get)
 print (topper)
-
-# Create variable first_name 
 
 # Create variable Last_name and store last two element in the list
 first_name = topper.split()[0]
 print(first_name)
 Last_name = topper.split()[1]
 print(Last_name)
+
 # Concatenate the string
 full_name = Last_name + ' ' + first_name
+
 # print the full_name
 print(full_name)
+
 # print the name in upper case
 certificate_name = full_name.upper()
 print(certificate_name)
+
 # Code ends here
 
 
